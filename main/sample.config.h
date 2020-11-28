495e17bd8a9592fc712c9f08b366f190cb6c02e1
Driant Naun <driantn@gmail.com>
2020-11-28 18:15:00 +0100

    udpated sample config and delay

--- main/sample.config.h
+++ main/sample.config.h
@@ -4,4 +4,16 @@
 // Definitions
 #define DHTPIN D2     // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-#define API_URL "api url"
+#define MQTT_VERSION MQTT_VERSION_3_1_1
+
+// MQTT: ID, server IP, port, username and password
+#define MQTT_CLIENT_ID "local-sensor"
+#define MQTT_SERVER_IP "192.168.0.15"
+#define MQTT_SERVER_PORT 1883
+#define MQTT_USER "user"
+#define MQTT_PASSWORD "password"
+#define MQTT_SENSOR_TOPIC "home/sensor/data"
+
+//Firebae config
+#define FIREBASE_HOST "someHost.firebaseio.com"
+#define FIREBASE_AUTH "2kjhdsh"
